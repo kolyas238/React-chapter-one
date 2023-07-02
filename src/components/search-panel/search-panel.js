dd2f29a9125c81406f1fe5da7cffe37249d1ff2b
kolyas238 <kolyas238@gmail.com>
2023-07-02 15:18:58 +0300

    initial commit with some structure

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,14 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input 
+            type="text" 
+            name="" 
+            id=""
+            className="form-control search-input"
+            placeholder="Найти сотрудника"/>
+    )
+}
+
+export default SearchPanel;
